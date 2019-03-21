22073401471f6c8d002c1e1a4426e94dd458b0b8
leroipanda <leroipanda@users.noreply.github.com>
2019-03-21 13:02:19 +0100

    maj
    reste à factoriser

--- allumettes/Arbitre.java
+++ allumettes/Arbitre.java
@@ -4,11 +4,13 @@ public class Arbitre {
 	
 	private Joueur Joueur1 ;
 	private Joueur Joueur2 ;
-	private int nbAllumetetsAvant;
+	//private int nbAllumetetsAvant;
 	private boolean TourJ1 = false ;
 	private boolean TricheDetecte = false ; 
+	private boolean arbitreConfiant ;
 	public Arbitre(Joueur j1,Joueur j2,boolean confiant) {
 		//on affecte les joueurs 
+		arbitreConfiant = confiant;
 		if(confiant) {
 		//System.out.println("je suis confiant");	
 		Joueur1 = j1;
@@ -22,17 +24,17 @@ public class Arbitre {
 		
 	}
 		public void arbitrer( jeuPrincipal Jeu){
-			while(Jeu.getNombreAllumettes() != 0){
+			while(Jeu.getNombreAllumettes() > 0){
 				//la partie est en cours : 
 				
 				//on alterne le tour du joueur 1 et 2 
 				
 				
-				int coup ; // variable qui stocke le coup du joueur avant verification 
+				int coup =0; // variable qui stocke le coup du joueur avant verification 
 				//c'est le tour du joueur 1
 				TourJ1 = true;
 				//on garde le nombre d'allumettes precedents
-				nbAllumetetsAvant =Jeu.getNombreAllumettes(); 
+				//nbAllumetetsAvant =Jeu.getNombreAllumettes(); 
 				
 				System.out.println("Nombre d'allumettes restantes : " + Jeu.getNombreAllumettes());
 				System.out.println("Au tour de "+Joueur1.getNom() );
@@ -41,24 +43,40 @@ public class Arbitre {
 				boolean sortir = false ;
 				do{
 					
-				coup  = Joueur1.getPrise(Jeu);
+				
 				sortir = true;
 					try{
+					 coup  = Joueur1.getPrise(Jeu);
 					 Jeu.retirer(coup);
 					 
 				     }
 					catch(CoupInvalideException e ){
 						sortir = false;
 					 }	
+					catch(OperationInterditeException e) {
+						if (!arbitreConfiant) {
+					
+						TricheDetecte = true;
+						System.out.println("Partie abandonnée car " +Joueur1.getNom() + " a triché !" );
+						
+						
+						}
+					}
 				
 				}while(!sortir);
 				
+				//on quite si triche 
+				if(TricheDetecte) {
+					break;
+				}
+				/*
 				//detection d'un tricheur 
 				if(antiTriche(nbAllumetetsAvant,Jeu)) {
 					TricheDetecte = true;
 					System.out.println("Partie abandonnée car " + Joueur1.getNom() + " a triché !" );
 					break;
 				}
+				*/
 				
 
 
@@ -71,26 +89,39 @@ public class Arbitre {
 					TourJ1 =false;
 					
 					//on garde le nombre d'allumettes precedents
-					nbAllumetetsAvant =Jeu.getNombreAllumettes(); 
+					//nbAllumetetsAvant =Jeu.getNombreAllumettes(); 
 					
 					System.out.println("Nombre d'allumettes restantes : " + Jeu.getNombreAllumettes());
 					System.out.println("Au tour de "+Joueur2.getNom() );
 					do{
 						
-						coup  = Joueur2.getPrise(Jeu);
+						
 						sortir = true;	
 						try{
+							coup  = Joueur2.getPrise(Jeu);
 							 Jeu.retirer(coup);
 						 }
 						 catch(CoupInvalideException e ){
 								sortir = false;
 							}
+						catch(OperationInterditeException e) {
+							if (!arbitreConfiant) {
+						
+							TricheDetecte = true;
+							System.out.println("Partie abandonnée car " +Joueur2.getNom() + " a triché !" );
+							}
+						}
 							
 						}while(!sortir);
-
+					
+					
+					//on quite si triche 
+					if(TricheDetecte) {
+						break;
+					}
 					
 					//detection d'un tricheur 
-					if(antiTriche(nbAllumetetsAvant,Jeu)) {
+					/* if(antiTriche(nbAllumetetsAvant,Jeu)) {
 						TricheDetecte = true;
 						System.out.println("Partie abandonnée car " +Joueur2.getNom() + " a triché !" );
 						break;
@@ -99,8 +130,9 @@ public class Arbitre {
 					 
 						System.out.println(Joueur2.getNom()+ " prend "+coup + " allumettes" );
 						System.out.println();
-				        }
+				        } */
 			
+			}
 			}
 			//la partie est fini 
 			//On regarde si il y a de la triche
@@ -116,10 +148,13 @@ public class Arbitre {
 			}
 			
 		}
-		public boolean antiTriche(int allumetteAvant ,jeuPrincipal Jeu){
+}
+		/* fonction inutile maintenant
+		 * public boolean antiTriche(int allumetteAvant ,jeuPrincipal Jeu){
+		
 			return allumetteAvant-Jeu.getNombreAllumettes() > 3;
 		}
-		
-	}
+		*/
+	
 
 
